737664c99c83dceebe1076e2c8b59f662717f47a
Basil ALBADER <baalbade@student.42.fr>
2024-05-17 09:21:49 +0200

    WIP

--- bootstrap_tutorial/js/routes.js
+++ bootstrap_tutorial/js/routes.js
@@ -1,12 +1,12 @@
 const routes = {
-    '/': '<h2>Home</h2><p>Welcome to the home page!</p>',
-    '/about': '<h2>About</h2><p>Learn more about us on this page.</p>',
-    '/whoWeAre': '<h2>Who We Are</h2><p>Get in touch with us!</p>',
-    '/login': '<h2>Login Page</h2><p>Authentication form comming soon</p>',
-    '/SignUp': '<h2>Sign Up Page</h2><p>Authentication form comming soon</p>',
+  '/': '<h2>Home</h2><p>Welcome to the home page!</p>',
+  '/about': '<h2>About</h2><p>Learn more about us on this page.</p>',
+  '/whoWeAre': '<h2>Who We Are</h2><p>Get in touch with us!</p>',
+  '/login': '<h2>Login Page</h2><p>Authentication form comming soon</p>',
+  '/SignUp': '<h2>Sign Up Page</h2><p>Authentication form comming soon</p>',
 };
 
 const renderRoute = (path) => {
-    const appDiv = document.getElementById('app');
-    appDiv.innerHTML = routes[path] || '<h2>404</h2><p>Page not found</p>';
+  const appDiv = document.getElementById('app');
+  appDiv.innerHTML = routes[path] || '<h2>404</h2><p>Page not found</p>';
 };
