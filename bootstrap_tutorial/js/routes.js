2860ee797ac6c353adb95cbb5f08b678a4053da4
Basil ALBADER <baalbade@student.42.fr>
2024-05-27 15:23:29 +0200

    WIP

--- bootstrap_tutorial/js/routes.js
+++ bootstrap_tutorial/js/routes.js
@@ -1,40 +1,41 @@
 'use strict'
 
 const routes = {
-    '': './home.js',
-    'home': './home.js',
-    'about': './about.js',
-    'whoWeAre': './whoWeAre.js',
-    'login': './login.js',
-    'SignUp': './pong.js',
+    '': '../js/home.js',
+    'home': '../js/home.js',
+    'about': '../js/about.js',
+    'whoWeAre': '../js/whoWeAre.js',
+    'login': '../js/login.js',
+    'SignUp': '../js/pong.js',
 };
 
-function navigateTo(path) {
-    const scriptPath = routes[path];
+const renderRoute = (path) => {
+    const scriptPath = routes[path] || routes['']; // use the default route if no script is found for the path
+    const appDiv = document.getElementById('app');
 
-    if (!scriptPath) {
-        console.error(`No script found for path ${path}`);
-        return;
+    // clear the appDiv's content before loading the new script
+    while (appDiv.firstChild) {
+        appDiv.removeChild(appDiv.firstChild);
     }
 
     fetch(scriptPath)
         .then(response => response.text())
         .then(scriptText => {
-            const appDiv = document.getElementById('app');
-            appDiv.innerHTML += scriptText;
+            const script = document.createElement('script');
+            script.textContent = scriptText;
+            // appDiv.appendChild(script);
         })
         .catch(error => {
             console.error(`Error loading script for ${path}: ${error}`);
         });
-}
+};
 
 document.addEventListener('DOMContentLoaded', () => {
     // load initial route
-    navigateTo('home');
+    renderRoute(window.location.pathname);
 
     // Handle navigation
     document.querySelectorAll('a[href]').forEach(anchor => {
-
         anchor.addEventListener('click', event => {
             event.preventDefault();
 
@@ -42,8 +43,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
             window.history.pushState({}, window.location.origin + '/' + path);
 
-            navigateTo(path);
+            renderRoute(path);
         });
     });
 
+    // Handle the browser's back and forward buttons
+    window.addEventListener('popstate', () => {
+        renderRoute(window.location.pathname);
+    });
+
+    // Call renderRoute whenever the pathname changes
+    setInterval(() => {
+        const currentPath = window.location.pathname;
+        if (currentPath !== lastPath) {
+            lastPath = currentPath;
+            renderRoute(currentPath);
+        }
+    }, 100);
+
+    let lastPath = window.location.pathname;
 });
