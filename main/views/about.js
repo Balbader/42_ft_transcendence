12152127c9dacfc0afaae37cccedb6755e717a46
Basil ALBADER <baalbade@student.42.fr>
2024-06-06 10:34:18 +0200

    new main folder for the app

---
+++ main/views/about.js
@@ -0,0 +1,4 @@
+'use strict'
+
+console.log('About Page script loaded!');
+document.getElementById('app').innerHTML += '<p>Welcome to the About page.</p>';
---
+++ main/views/home.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let app = document.getElementById('app');
+let header = document.createElement('div');
+let div = `<div class=""></div>`
+
+app.appendChild(header);
