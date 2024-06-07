be8a47e5b68dc27d51b541121c06713c1d906c6c
balbader <basilalbapro@gmail.com>
2024-06-07 23:32:22 +0200

    WIP

--- main/app.js
+++ main/app.js
@@ -1,36 +1,38 @@
 import { renderRoute, router } from "./lib/Router/routes.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // load initial route
+  // renderRoute(window.location.pathname);
 
-    // load initial route
-    // renderRoute(window.location.pathname);
+  // Handle navigation
+  document.addEventListener(
+    "click",
+    (event) => {
+      const isAnchor = event.target.nodeName === "A";
+      console.log(event.target);
+      console.log("isAnchor: ", isAnchor);
 
-    // Handle navigation
-    document.addEventListener("click", (event) => {
+      if (!isAnchor) return;
+      console.log("isAnchor: ", isAnchor);
 
-        const isAnchor = event.target.nodeName === 'a';
-        console.log(event.target);
-        console.log('isAnchor: ', isAnchor);
+      event.preventDefault();
 
-        if (!isAnchor) return;
-        console.log('isAnchor: ', isAnchor);
+      const path = event.target.getAttribute("href");
 
-        event.preventDefault();
+      router.goTo(path);
 
-        const path = event.target.getAttribute('href');
+      renderRoute(path);
+    },
+    true
+  );
 
-        router.goTo(path);
-
-        renderRoute(path);
-    }, true);
-
-    // Handle the browser's back and forward buttons
-    // window.addEventListener("popstate", (event) => {
-    //     const path =
-    //         event.state && event.state.path
-    //             ? event.state.path
-    //             : window.location.pathname;
-    //
-    //     renderRoute(path);
-    // });
+  // Handle the browser's back and forward buttons
+  // window.addEventListener("popstate", (event) => {
+  //     const path =
+  //         event.state && event.state.path
+  //             ? event.state.path
+  //             : window.location.pathname;
+  //
+  //     renderRoute(path);
+  // });
 });
--- main/lib/Router/routes.js
+++ main/lib/Router/routes.js
@@ -1,44 +1,45 @@
 const routes = {
-    "": "views/home.js",
-    home: "views/home.js",
-    about: "views/about.js",
-    whoWeAre: "views/whoWeAre.js",
-    pong: "views/pong.js",
-    login: "views/login.js",
+  "": "views/home.js",
+  home: "views/home.js",
+  about: "views/about.js",
+  whoWeAre: "views/whoWeAre.js",
+  pong: "views/pong.js",
+  login: "views/login.js",
 };
 
 const Router = () => {
-    const state = ["home"];
+  const state = ["home"];
 
-    const goTo = (path) => {
-        state.push(path);
-    };
+  const goTo = (path) => {
+    state.push(path);
+  };
 
-    const goBack = () => {
-        if (state.length === 1) return;
-        state.pop();
-    };
+  const goBack = () => {
+    if (state.length === 1) return;
+    state.pop();
+  };
 
-    const currentRoute = () => {
-        return state.at(-1);
-    };
+  const currentRoute = () => {
+    return state.at(-1);
+  };
 
-    return {
-        goTo,
-        goBack,
-        currentRoute,
-    };
+  return {
+    goTo,
+    goBack,
+    currentRoute,
+  };
 };
 
 const router = Router();
 
 const renderRoute = async () => {
-    const scriptPath = routes[router.currentRoute()]; // use the default route if no script is found for the path
-    const appDiv = document.getElementById('app');
-    console.log(appDiv);
-    const view = await import(scriptPath);
-    console.log(view);
-    appDiv.innerHTML = view();
+  const scriptPath = routes[router.currentRoute()]; // use the default route if no script is found for the path
+  const appDiv = document.getElementById("app");
+  const view = await import(`http://localhost:3000/${scriptPath}`);
+  const finalView = view.default();
+  typeof finalView === "string"
+    ? (appDiv.innerHTML = finalView)
+    : appDiv.replaceChild(view.default(), appDiv.children[0]);
 };
 
 export { renderRoute, router };
