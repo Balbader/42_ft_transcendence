88649d3d02c3ebe16f6f89e92446b452f05b99c2
balbader <basilalbapro@gmail.com>
2024-06-08 17:36:06 +0200

    WIP

--- django/fortuneteller/fortuneteller/settings.py
+++ django/fortuneteller/fortuneteller/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'randomfortune.app.RandomfortuneConfig',
+    'randomfortune.apps.RandomfortuneConfig',
 ]
 
 MIDDLEWARE = [
