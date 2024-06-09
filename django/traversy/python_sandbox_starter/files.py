7d5f9b1ef3c6a9d885f6b6b33d6de3f8a06523e3
Basil ALBADER <baalbade@student.42.fr>
2024-06-09 11:22:52 +0200

    WIP

--- django/traversy/python_sandbox_starter/files.py
+++ django/traversy/python_sandbox_starter/files.py
@@ -1,1 +1,18 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file
+myFile = open('myFile.txt', 'w')
+
+# Get some info on the file
+print('Name :', myFile.name)
+print('Is Closed :', myFile.closed)
+print('Opening Mode :', myFile.mode)
+
+# Write to file
+myFile.write('I love Python')
+myFile.write(' and JavaScript')
+myFile.close()
+
+# Append to file: if we don't Append, it will overwrite what was written in the file previously
+myFile = open('myFile.txt', 'a')
+
