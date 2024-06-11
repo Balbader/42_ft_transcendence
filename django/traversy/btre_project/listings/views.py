99e9b8e68a362f95f75debecb736b35d07d2f3a5
balbader <basilalbapro@gmail.com>
2024-06-11 18:22:05 +0200

    WIP

--- django/traversy/btre_project/listings/views.py
+++ django/traversy/btre_project/listings/views.py
@@ -17,8 +17,10 @@ def index(request):
 
     return render(request, 'listings/listings.html', context)
 
+
 def listing(request, listing_id):
     return render(request, 'listings/listing.html')
+
 
 def search(request):
     return render(request, 'listings/search.html')
