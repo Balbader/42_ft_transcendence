04017d1f3bde4c54c1099979876d51b0c5c04940
balbader <basilalbapro@gmail.com>
2024-06-13 14:41:14 +0200

    WIP

--- transcendence_components_lib/src/components/Login_Form/LoginForm.js
+++ transcendence_components_lib/src/components/Login_Form/LoginForm.js
@@ -7,14 +7,17 @@ class LoginForm {
     renderForm() {
         this.container.innerHTML = `
             <form id="loginForm" class="mt-3">
+
                 <div class="form-group">
                     <label for="loginEmail">Email address</label>
                     <input type="email" class="form-control" id="loginEmail" required>
                 </div>
+
                 <div class="form-group">
                     <label for="loginPassword">Password</label>
                     <input type="password" class="form-control" id="loginPassword" required>
                 </div>
+
                 <button type="submit" class="btn btn-primary">Login</button>
             </form>
         `;
