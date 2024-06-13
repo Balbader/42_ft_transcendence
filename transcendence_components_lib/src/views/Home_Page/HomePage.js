2ea2da186fa424e3b4f9645e5e5417c4f2584d35
balbader <basilalbapro@gmail.com>
2024-06-13 18:55:48 +0200

    WIP

---
+++ transcendence_components_lib/src/views/Home_Page/HomePage.js
@@ -0,0 +1,54 @@
+class HomePage {
+
+    constructor(container) {
+        this.container = container;
+    }
+
+    renderForm() {
+        this.container.innerHTML = `
+            <section id="loginForm" class="vh-100 gradient-custom">
+                <div class="container py-5 h-100">
+                    <div class="row d-flex justify-content-center align-items-center h-100">
+                        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                            <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                                <div class="card-body p-5 text-center">
+                                    <div class="mb-md-5 mt-md-4 pb-5">
+                                        <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                                        <p class="text-white-50 mb-5">Please enter your login and password!</p>
+
+                                        <!--NOTE: Email Field -->
+                                        <div data-mdb-input-init class="form-outline form-white mb-4">
+                                            <input type="email" id="loginEmail" class="form-control form-control-lg" />
+                                            <label class="form-label" for="">Email</label>
+                                        </div>
+
+                                        <!--NOTE: Password Field -->
+                                        <div data-mdb-input-init class="form-outline form-white mb-4">
+                                            <input id="loginPassword" type="password" id="typePasswordX" class="form-control form-control-lg" />
+                                            <label class="form-label" for="">Password</label>
+                                        </div>
+
+                                        <!--NOTE: Reset Password link -->
+                                        <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+
+                                        <!--NOTE: Login Btn -->
+                                        <button id="btnLogin" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+                                        <!--NOTE: Login with 42 Btn -->
+                                        <button id="btnFortyTwo" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5" type="submit">Login with 42</button>
+                                    </div>
+                                    <div>
+                                        <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold"> Sign Up</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        `;
+    }
+}
+
+export default HomePage;
