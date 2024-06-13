3728d8fb7d5ab666ba9f307004c581df7a2d9fb8
balbader <basilalbapro@gmail.com>
2024-06-13 13:13:58 +0200

    WIP

---
+++ transcendence_components_lib/src/components/Button.js
@@ -0,0 +1,17 @@
+class Button {
+    constructor(text, onClick) {
+        this.text = text;
+        this.onClick = onClick;
+    }
+
+    render() {
+        const button = document.createElement('button');
+
+        button.innerText = this.text;
+        button.addEventListener('click', this.onClick);
+
+        return button;
+    }
+}
+
+export default Button;
---
+++ transcendence_components_lib/src/main.js
@@ -0,0 +1,8 @@
+import { Button, Card } from './index.js'
+
+const app = document.getElementById('app');
+const button = new Button('Click Me', () => alert('Button clicked.'));
+const card = new Card('Card Title', 'This is the card content.');
+
+app.appendChild(button.render());
+app.appendChild(card.render());
