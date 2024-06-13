d5db2aa2fe6cabb7536c77330f746147dae4ff0f
balbader <basilalbapro@gmail.com>
2024-06-13 15:24:18 +0200

    WIP

--- transcendence_components_lib/src/components/index.js
+++ transcendence_components_lib/src/components/index.js
@@ -1,5 +1,4 @@
 import LoginForm from './Login_Form/LoginForm.js';
-import LoginForm2 from './Login_Form/LoginForm2.js';
 import SignUpForm from './SignUp_Form/SignUpForm.js';
 
-export { LoginForm, LoginForm2, SignUpForm };
+export { LoginForm, SignUpForm };
