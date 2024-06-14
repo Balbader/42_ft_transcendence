ff2f01bf97ddf4625c172f2ac1c084f3bdf2f266
balbader <basilalbapro@gmail.com>
2024-06-14 12:36:43 +0200

    WIP

--- django/traversy/btre_project/accounts/views.py
+++ django/traversy/btre_project/accounts/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing err message')
-        return redirect('register')
+
+        # NOTE : Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # NOTE : Check if passwords match
+        if password == password2:
+            if User.objects.filter(username = username).exists():
+                messages.error(request, 'User Name Already Taken!')
+                return redirect('register')
+            else:
+                if User.objects.filter(email = email).exists():
+                    messages.error(request, 'Email Already In Use!')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+                    # NOTE: option 1
+                    # auth.login(request, user)
+                    # messages.success(request, 'User Successfuly Created! Login Successful.')
+                    # return redirect('index')
+                    user.save()
+                    messages.success(request, 'Registration Successful!')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords Do Not Match!')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
