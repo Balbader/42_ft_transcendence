4237e439f51cf936c0c125cb36694f43048c2807
balbader <basilalbapro@gmail.com>
2024-06-15 17:51:38 +0200

    WIP

--- main/src/views/home.js
+++ main/src/views/home.js
@@ -2,14 +2,13 @@ import { createElement } from "../lib/create-element.js";
 import rest from "../lib/rest.js";
 
 const home = () => {
-    const button = createElement("button", { class: "btn btn-primary" })
+    const button = createElement("button", { class: "btn btn-outline-dark" })
         .appendText("Load first todo")
-        .on("click", async (event_) => {
-            const firstTodo = await rest.get("todos").json();
-            console.log("Todo loaded:", firstTodo);
-            const div = event_.target.parentElement;
-            div.textContent =
-                firstTodo.title + " is completed? " + firstTodo.completed;
+        .on("click", async () => {
+            const todos = await rest.get("todos").json();
+            console.log("Todo loaded:", todos);
+            button.textContent =
+                todos.title + " is completed? " + todos.completed;
         });
 
     // const Home = createElement("div", null, button)
