835b4ad01a756e50659a6949134a72bc98d2c64c
balbader <basilalbapro@gmail.com>
2024-06-15 13:06:34 +0200

    add Contact admin to admin page

--- django/traversy/btre_project/contacts/admin.py
+++ django/traversy/btre_project/contacts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('listing', 'listing_id', 'name', 'user_id', 'email', 'phone', 'message', 'contact_date')
+    list_display_links = ('user_id', 'name')
+    search_fields = ('name', 'user_id', 'email', 'listing')
+    list_per_page = 10
+
+admin.site.register(Contact, ContactAdmin)
