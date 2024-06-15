004311cbb8a9e012e1161c23222d0200df4957db
balbader <basilalbapro@gmail.com>
2024-06-15 18:55:15 +0200

    WIP

--- main/src/views/home.js
+++ main/src/views/home.js
@@ -1,12 +1,6 @@
 import { createElement } from '../lib/create-element.js';
 import rest from '../lib/rest.js';
 
-// Const header = createElement("div", null, button)
-//     .setStyle({ background: "lightGrey", height: "200px", width: "300px" })
-//     .on("click", (event_) => {
-//         event_.target.setStyle({ background: "lightgreen" });
-//     });
-
 const home = () => {
     const button = createElement('button', { class: 'btn btn-outline-dark' })
         .appendText('Load first todo')
