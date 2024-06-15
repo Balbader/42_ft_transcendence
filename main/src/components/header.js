0dc0a01d4c3525d1c612b3a4d978f06698fb6498
balbader <basilalbapro@gmail.com>
2024-06-15 18:52:49 +0200

    WIP

--- main/src/components/header.js
+++ main/src/components/header.js
@@ -2,8 +2,6 @@ import { createElement } from '../lib/create-element.js';
 
 const header = () => {
     const appDiv = document.querySelector('#app');
-    console.log('header');
-    console.log('appDiv', appDiv);
     const headerTag = createElement('section').appendText('hello header');
     appDiv.append(headerTag);
 };
