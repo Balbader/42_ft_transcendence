83874a656a3a1320f624649584d69e44f55d3b8b
balbader <basilalbapro@gmail.com>
2024-06-15 17:14:40 +0200

    WIP

--- main/src/views/home.js
+++ main/src/views/home.js
@@ -2,7 +2,7 @@ import { createElement } from "../lib/create-element.js";
 import rest from "../lib/rest.js";
 
 const home = () => {
-    const button = createElement("button", { class: "btn" })
+    const button = createElement("button", { class: "btn btn-primary" })
         .appendText("Load first todo")
         .on("click", async (event_) => {
             const firstTodo = await rest.get("todos").json();
@@ -12,13 +12,13 @@ const home = () => {
                 firstTodo.title + " is completed? " + firstTodo.completed;
         });
 
-    const Home = createElement("div", null, button)
-        .setStyle({ background: "lightGrey", height: "200px", width: "300px" })
-        .on("click", (event_) => {
-            event_.target.setStyle({ background: "lightgreen" });
-        });
+    // const Home = createElement("div", null, button)
+    //     .setStyle({ background: "lightGrey", height: "200px", width: "300px" })
+    //     .on("click", (event_) => {
+    //         event_.target.setStyle({ background: "lightgreen" });
+    //     });
 
-    return Home;
+    return button;
 };
 
 export default home;
