4fcf51c76e697e13d285a61317863e489acb8ae9
balbader <basilalbapro@gmail.com>
2024-06-15 18:05:39 +0200

    WIP

--- main/src/views/home.js
+++ main/src/views/home.js
@@ -1,21 +1,26 @@
 import { createElement } from "../lib/create-element.js";
 import rest from "../lib/rest.js";
 
+
+// const Home = createElement("div", null, button)
+//     .setStyle({ background: "lightGrey", height: "200px", width: "300px" })
+//     .on("click", (event_) => {
+//         event_.target.setStyle({ background: "lightgreen" });
+//     });
+
 const home = () => {
     const button = createElement("button", { class: "btn btn-outline-dark" })
         .appendText("Load first todo")
         .on("click", async () => {
             const todos = await rest.get("todos").json();
-            console.log("Todo loaded:", todos);
-            button.textContent =
-                todos.title + " is completed? " + todos.completed;
+
+            todos.forEach(todo => {
+                console.log(`${todo.id} : ${todo.title} - ${todo.completed}`);
+            });
+
+            button.textContent = todos[0].id + " is completed? " + todos[0].completed;
         });
 
-    // const Home = createElement("div", null, button)
-    //     .setStyle({ background: "lightGrey", height: "200px", width: "300px" })
-    //     .on("click", (event_) => {
-    //         event_.target.setStyle({ background: "lightgreen" });
-    //     });
 
     return button;
 };
