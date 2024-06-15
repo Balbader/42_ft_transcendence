fac5be5000eb76053e17f331e91eb5b40d887228
balbader <basilalbapro@gmail.com>
2024-06-15 09:49:24 +0200

    WIP

--- main/src/components/header.js
+++ main/src/components/header.js
@@ -1,11 +1,15 @@
-import {createElement} from '../lib/create-element.js';
+import { createElement } from '../lib/create-element.js';
 
 const header = () => {
-	const appDiv = document.querySelector('#app');
-	console.log('header');
-	console.log('appDiv', appDiv);
-	const headerTag = createElement('section').appendText('hello header');
-	appDiv.append(headerTag);
+  const appDiv = document.querySelector('#app');
+  console.log('header');
+  console.log('appDiv', appDiv);
+  const headerTag = createElement('section').appendText('hello header');
+  appDiv.append(headerTag);
 };
 
-export {header};
+export { header };
+
+const hello = () => {
+  return hello;
+}
