295fa66f3124c2febd57ee80c373ad90b3388074
balbader <basilalbapro@gmail.com>
2024-06-16 16:50:59 +0200

    WIP

--- vite-lit/src/my-panel.js
+++ vite-lit/src/my-panel.js
@@ -11,9 +11,11 @@ export class MyPanel extends LitElement {
       background: var(--my-panel-primary-bg, black);
       color: var(--my-panel-primary-color, yellow);
       padding: 0.8rem;
-      border: 1px solid yellow;
-      border-top-left-radius: 1rem;
-      border-top-right-radius: 1rem;
+      border: 2px solid orange;
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
+      border-bottom-left-radius: 5px;
+      border-bottom-right-radius: 5px;
 
       display: flex;
       justify-content: space-between;
@@ -24,25 +26,34 @@ export class MyPanel extends LitElement {
       background: aquamarine;
       color: blue;
       padding: 1rem;
-      border: 1px solid red;
+      border: 2px solid black;
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
+      border-bottom-left-radius: 5px;
+      border-bottom-right-radius: 5px;
     }
   `;
 
   constructor() {
     super();
-    // this.title = "Home Sweet Home";
+    this.title = "Home Sweet Home";
   }
 
   render() {
     return html`
       <div>
-        <div class="title">
+        <div class="title container-fluid">
           ${this.title}
           <div>🖕</div>
         </div>
-        <div class="body">
-          <slot> </slot>
+        <div class="body" mr-2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+          accusantium nemo autem.
         </div>
+        <button class="btn btn-outline-dark">Click Me</button>
       </div>
     `;
   }
