ff700ee099c3722a8e737c5f1594d16e459e2082
balbader <basilalbapro@gmail.com>
2024-06-17 01:20:57 +0200

    dist folder

--- FT_TRANSCENDENCE/front/dist/tools/create-element.js
+++ FT_TRANSCENDENCE/front/dist/tools/create-element.js
@@ -1,49 +1,47 @@
-const createElement = (tagName, attributes, ...children) => {
-  const element = document.createElement(tagName);
-  for (const [key, value] of Object.entries(attributes || {})) {
-    element.setAttribute(key, value);
-  }
-  element.append(...children);
-  element.setStyle = styles => {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+/** @jsx createElement */
+var createElement = function createElement(tag, attributes) {
+  var prop = document.createElement(tag);
+  Object.keys(attributes || {}).forEach(function (key) {
+    if (key === "style") {
+      Object.keys(attributes[key]).forEach(function (keyStyle) {
+        prop.style[keyStyle] = prop[key][keyStyle];
+      });
+    } else {
+      prop[key] = attributes[key];
+    }
+  });
+  var addChild = function addChild(child) {
+    if (Array.isArray(child)) {
+      child.forEach(function (c) {
+        return addChild(c);
+      });
+    } else if (_typeof(child) === "object") {
+      prop.appendChild(child);
+    } else {
+      prop.appendChild(document.createTextNode(child));
+    }
+  };
+  prop.on = function (eventType, eventHandler, options) {
+    prop.addEventListener(eventType, eventHandler, options);
+    return prop;
+  };
+  prop.setStyle = function (styles) {
     Object.assign(element.style, styles);
-    return element;
+    return prop;
   };
-  element.appendText = text => {
-    element.append(text);
-    return element;
-  };
-  element.on = (eventType, eventHandler, options) => {
-    element.addEventListener(eventType, eventHandler, options);
-    return element;
-  };
-  return element;
+  for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    children[_key - 2] = arguments[_key];
+  }
+  (children || []).forEach(function (c) {
+    return addChild(c);
+  });
+  return prop;
 };
-
-// const createElement = (tagName, attributes, ...children) => {
-//   const element = document.createElement(tagName);
-
-//   for (const [key, value] of Object.entries(attributes || {})) {
-//     element.setAttribute(key, value);
-//   }
-
-//   element.append(...children);
-
-//   element.setStyle = (styles) => {
-//     Object.assign(element.style, styles);
-//     return element;
-//   };
-
-//   element.appendText = (text) => {
-//     element.append(text);
-//     return element;
-//   };
-
-//   element.on = (eventType, eventHandler, options) => {
-//     element.addEventListener(eventType, eventHandler, options);
-//     return element;
-//   };
-
-//   return element;
-// };
-
-export { createElement };
+var _default = exports["default"] = createElement;
--- FT_TRANSCENDENCE/front/dist/tools/rest.js
+++ FT_TRANSCENDENCE/front/dist/tools/rest.js
@@ -1,5 +1,12 @@
-import ky from "https://esm.sh/ky@1";
-const rest = ky.create({
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _ky = _interopRequireDefault(require("https://esm.sh/ky@1"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var rest = _ky["default"].create({
   prefixUrl: "https://jsonplaceholder.typicode.com/"
 });
-export default rest;
+var _default = exports["default"] = rest;
