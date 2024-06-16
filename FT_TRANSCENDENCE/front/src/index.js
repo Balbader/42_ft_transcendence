b2aa281e1b37261db616e85c89a336573d708c72
balbader <basilalbapro@gmail.com>
2024-06-17 01:15:34 +0200

    test: create button and display on index.hstml

--- FT_TRANSCENDENCE/front/src/index.js
+++ FT_TRANSCENDENCE/front/src/index.js
@@ -1,1 +1,16 @@
-console.log("hello from index");
+/** @jsx createElement */
+import createElement from "./tools/create-element.js";
+
+const app = document.getElementById("app");
+
+const handleClick = () => {
+  console.log("Button was clicked!");
+};
+
+const button = createElement(
+  "button",
+  { className: "btn btn-primary" },
+  "Click Me"
+).on("click", handleClick);
+
+app.appendChild(button);
