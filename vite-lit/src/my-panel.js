d811d26369b8a6e83e4b45c8f57bda26cd579e79
balbader <basilalbapro@gmail.com>
2024-06-16 15:05:26 +0200

    WIP

--- vite-lit/src/my-panel.js
+++ vite-lit/src/my-panel.js
@@ -1,10 +1,39 @@
-import { LitElement, html } from 'lit'
+import { LitElement, html, css } from 'lit'
 
 export class MyPanel extends LitElement {
+
+    static styles = css`
+        .title {
+            background: black;
+            color: yellow;
+            padding: 0.8rem;
+            border: 1px solid yellow;
+            border-top-left-radius: 1rem;
+            border-top-right-radius: 1rem;
+
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .body {
+            background: aquamarine;
+            color: blue;
+            padding: 1rem;
+            border: 1px solid red;
+        }
+    `
+
     render() {
         return html`
             <div>
-                Hello Lit
+                <div class="title">
+                    Fuck you!
+                    <div>🖕</div>
+                </div>
+                <div class="body">
+                    CONTENT HERE
+                </div>
             </div>
         `
     }
