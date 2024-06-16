3dc010e6d09c0295b2b1e0781ca6844cfa7b06ae
balbader <basilalbapro@gmail.com>
2024-06-16 09:12:12 +0200

    WIP

--- main/src/views/ui-lib.js
+++ main/src/views/ui-lib.js
@@ -2,22 +2,23 @@ import { createElement } from '../lib/create-element.js';
 
 const ui_lib = () => {
 
-    const btnPrimary = (text) => {
+    const btnPrimary = () => {
         createElement(
             'button',
             { class: `btn btn-outline-dark` },
-        ).appendText(text).on('click', async () => {
-            console.log('Hello from Canvas.');
-        });
+        )
+            .appendText('Login')
+            .on('click', async () => {
+                console.log('Hello from Canvas.');
+            });
     };
 
     const appDiv = createElement(
         'div',
         { class: 'container' },
-    );
+    ).appendText('div');
 
-    appDiv.append(btnPrimary('Login'));
-    console.log(appDiv)
+    appDiv.append(btnPrimary);
     return appDiv;
 }
 
