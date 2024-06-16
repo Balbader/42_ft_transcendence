ecaf6387a89d10bb310fa9474152a9af3a07e67a
balbader <basilalbapro@gmail.com>
2024-06-16 10:58:23 +0200

    WIP

--- main/dist/views/who-we-are.js
+++ main/dist/views/who-we-are.js
@@ -4,8 +4,9 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
+var _createElement = require("../lib/create-element.js");
 var whoWeAre = function whoWeAre() {
-  var div = document.createElement('div');
+  var div = (0, _createElement.createElement)('div');
   var hello = /*#__PURE__*/React.createElement("p", null, "hello from the whoWeAre page.");
   div.appendChild(hello);
   return div;
