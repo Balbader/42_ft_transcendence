477437728672497dc01edb788e8d62b3de0edc80
balbader <basilalbapro@gmail.com>
2024-06-17 17:57:26 +0200

    WIP

--- FT_TRANSCENDENCE/Client/src/my-element/my-element.js
+++ FT_TRANSCENDENCE/Client/src/my-element/my-element.js
@@ -11,13 +11,11 @@ import "../components/buttons/test-button/test-button.js";
 export class MyElement extends LitElement {
   static get styles() {
     const { cssRules } = document.styleSheets[0];
-
     const globalStyle = css([
       Object.values(cssRules)
         .map((rule) => rule.cssText)
         .join("\n"),
     ]);
-
     return [globalStyle, css``];
   }
 
