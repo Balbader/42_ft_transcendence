0a0baa796537fc048295e19d8925b3b8366c2ffc
balbader <basilalbapro@gmail.com>
2024-06-17 12:42:03 +0200

    WIP

--- FT_TRANSCENDENCE/Client/src/index.js
+++ FT_TRANSCENDENCE/Client/src/index.js
@@ -1,4 +1,5 @@
 /** @jsx createElement */
+
 import createElement from "./tools/create-element.js";
 
 const app = document.getElementById("app");
@@ -7,10 +8,15 @@ const handleClick = () => {
   console.log("Button was clicked!");
 };
 
-const button = createElement(
-  "button",
-  { className: "btn btn-primary" },
-  "Click Me"
-).on("click", handleClick);
+const button = ({ variant = "btn btn-outline-darck", text = "Click Me" }) =>
+  createElement("button", `${variant}`, `${text}`).on("click", handleClick);
 
+const textBlock = createElement(
+  "div",
+  "class=container container-fluid",
+  "Hello World"
+);
+
+app.appendChild(textBlock);
 app.appendChild(button);
+console.log(app);
