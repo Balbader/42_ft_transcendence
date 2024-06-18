bebdfee8b7b9b55e60908f19b854b674fa84c107
balbader <basilalbapro@gmail.com>
2024-06-18 10:11:56 +0200

    add sign up page component

---
+++ FT_TRANSCENDENCE/Client/src/views-static/sign-up-page.js
@@ -0,0 +1,162 @@
+import { LitElement, html, css } from "lit";
+
+export class SignUpPage extends LitElement {
+  static get styles() {
+    const { cssRules } = document.styleSheets[0];
+
+    const globalStyle = css([
+      Object.values(cssRules)
+        .map((rule) => rule.cssText)
+        .join("\n"),
+    ]);
+
+    return [globalStyle, css``];
+  }
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <section class="vh-100" style="background-color: #eee;">
+        <div class="container h-100">
+          <div
+            class="row d-flex justify-content-center align-items-center h-100"
+          >
+            <div class="col-lg-12 col-xl-11">
+              <div class="card text-black" style="border-radius: 25px;">
+                <div class="card-body p-md-5">
+                  <div class="row justify-content-center">
+                    <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                      <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                        Sign up
+                      </p>
+
+                      <form class="mx-1 mx-md-4">
+                        <div class="d-flex flex-row align-items-center mb-4">
+                          <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                          <div
+                            data-mdb-input-init
+                            class="form-outline flex-fill mb-0"
+                          >
+                            <input
+                              type="text"
+                              id="form3Example1c"
+                              class="form-control"
+                            />
+                            <label class="form-label" for="form3Example1c"
+                              >Your Name</label
+                            >
+                          </div>
+                        </div>
+
+                        <div class="d-flex flex-row align-items-center mb-4">
+                          <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                          <div
+                            data-mdb-input-init
+                            class="form-outline flex-fill mb-0"
+                          >
+                            <input
+                              type="email"
+                              id="form3Example3c"
+                              class="form-control"
+                            />
+                            <label class="form-label" for="form3Example3c"
+                              >Your Email</label
+                            >
+                          </div>
+                        </div>
+
+                        <div class="d-flex flex-row align-items-center mb-4">
+                          <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                          <div
+                            data-mdb-input-init
+                            class="form-outline flex-fill mb-0"
+                          >
+                            <input
+                              type="password"
+                              id="form3Example4c"
+                              class="form-control"
+                            />
+                            <label class="form-label" for="form3Example4c"
+                              >Password</label
+                            >
+                          </div>
+                        </div>
+
+                        <div class="d-flex flex-row align-items-center mb-4">
+                          <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                          <div
+                            data-mdb-input-init
+                            class="form-outline flex-fill mb-0"
+                          >
+                            <input
+                              type="password"
+                              id="form3Example4cd"
+                              class="form-control"
+                            />
+                            <label class="form-label" for="form3Example4cd"
+                              >Repeat your password</label
+                            >
+                          </div>
+                        </div>
+
+                        <div
+                          class="form-check d-flex justify-content-center mb-5"
+                        >
+                          <input
+                            class="form-check-input me-2"
+                            type="checkbox"
+                            value=""
+                            id="form2Example3c"
+                          />
+                          <label class="form-check-label" for="form2Example3">
+                            I agree all statements in
+                            <a href="#!">Terms of service</a>
+                          </label>
+                        </div>
+
+                        <div
+                          class="d-flex justify-content-center mx-4 mb-3 mb-lg-4"
+                        >
+                          <button
+                            type="button"
+                            data-mdb-button-init
+                            data-mdb-ripple-init
+                            class="btn btn-primary btn-lg"
+                          >
+                            Register
+                          </button>
+
+                          <button
+                            type="button"
+                            data-mdb-button-init
+                            data-mdb-ripple-init
+                            class="btn btn-dark bth-lg"
+                          >
+                            Sign Up With 42
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                    <div
+                      class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2"
+                    >
+                      <img
+                        src="./src/components/nav-bar/nav-bar-assets/ping-pong.png"
+                        class="img-fluid"
+                        alt="Pong Logo"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    `;
+  }
+}
+customElements.define("sign-up-page", SignUpPage);
