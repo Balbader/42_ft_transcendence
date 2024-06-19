7836d88265a000f571212b86aabe8f01484b6d22
balbader <basilalbapro@gmail.com>
2024-06-19 16:31:59 +0200

    update

--- FT_TRANSCENDENCE/Client/src/views/pwd-change-page/password-change-page.js
+++ FT_TRANSCENDENCE/Client/src/views/pwd-change-page/password-change-page.js
@@ -1,9 +1,15 @@
+import "../../components/nav-bar/nav-bar-in.js";
 import "../../components/password/password-change-component.js";
+import "../../components/footer/footer-in.js";
 import { LitElement, html, css } from "lit";
 
 export class PasswordChangePage extends LitElement {
   render() {
-    return html` <password-change-component></password-change-component> `;
+    return html`
+      <nav-bar-in></nav-bar-in>
+      <password-change-component></password-change-component>
+      <footer-in></footer-in>
+    `;
   }
 }
 customElements.define("password-change-page", PasswordChangePage);
