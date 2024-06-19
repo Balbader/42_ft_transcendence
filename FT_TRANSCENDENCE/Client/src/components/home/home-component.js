c2e341748e3d8235627fdeb477e160e7062af96c
balbader <basilalbapro@gmail.com>
2024-06-19 16:16:48 +0200

    add home-component.js

---
+++ FT_TRANSCENDENCE/Client/src/components/home/home-component.js
@@ -0,0 +1,15 @@
+import "../nav-bar/nav-bar-out.js";
+import "../signup/sign-up-component.js";
+import "../footer/footer-out.js";
+import { LitElement, html, css } from "lit";
+
+export class HomeComponent extends LitElement {
+  render() {
+    return html`
+      <nav-bar-out></nav-bar-out>
+      <sign-up-component></sign-up-component>
+      <footer-out></footer-out>
+    `;
+  }
+}
+customElements.define("home-component", HomeComponent);
