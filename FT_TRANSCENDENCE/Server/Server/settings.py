8a9509be4b684b661c35b1f9114c850cc5a4d250
balbader <basilalbapro@gmail.com>
2024-06-20 10:45:13 +0200

    update settings.py

--- FT_TRANSCENDENCE/Server/Server/settings.py
+++ FT_TRANSCENDENCE/Server/Server/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+		'auth.appsl.AuthConfig',
+		'templates.appsl.TemplatesConfig',
 ]
 
 MIDDLEWARE = [
