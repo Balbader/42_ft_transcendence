a2a03c5ae59c61d44d8074944cb489ae6c2d7901
balbader <basilalbapro@gmail.com>
2024-06-21 14:37:00 +0200

    change file location

--- main/dist/components/header.js
+++
@@ -1,7 +0,0 @@
-import { createElement } from '../lib/create-element.js';
-const header = () => {
-  const appDiv = document.querySelector('#app');
-  const headerTag = createElement('section').appendText('hello header');
-  appDiv.append(headerTag);
-};
-export { header };
