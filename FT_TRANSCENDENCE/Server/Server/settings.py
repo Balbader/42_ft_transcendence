8bd31dc74a3fa149960f1bfe72a84b91e36a7d0c
balbader <basilalbapro@gmail.com>
2024-06-21 16:50:40 +0200

    settings and urls for main server app

--- FT_TRANSCENDENCE/Server/Server/settings.py
+++ FT_TRANSCENDENCE/Server/Server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+		'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -51,10 +53,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Server.urls'
 
+import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
