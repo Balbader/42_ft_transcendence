d22c6dcb1e8d1fa000e79fef60550c4896a4f4b6
balbader <basilalbapro@gmail.com>
2024-06-22 13:46:06 +0200

    WIP

--- FT_TRANSCENDENCE/Client/src/tools/router/router.js
+++ FT_TRANSCENDENCE/Client/src/tools/router/router.js
@@ -1,15 +1,59 @@
-import { BrowserRoute } from 'vanilla-routing';
+import about from "../../../views/about.js";
+import home from "../../../views/home.js";
+import whoWeAre from "../../../views/who-we-are.js";
+import login from "../../../views/login.js";
+import ui_lib from "../../views/ui-lib.js";
 
-// Define your routes here
-const routes = [
-	{
-		'pathname': '/',
-		element: () => document.createElement('div') {/* 👈 Renders at / */ }
-  },
-  {
-    'pathname': '/post',
-		element: () => document.createElement('div') {/* 👈 Renders at /post */ }
-  },
-];
+const routes = {
+  home,
+  about,
+  whoWeAre,
+  login,
+  ui_lib,
+};
 
-BrowserRoute(routes); // Initialize browser-based routing
+const createRouter = () => {
+  let state = [window.location.pathname.replace("/", "home")];
+
+  const goTo = (path) => {
+    state.push(path);
+    window.history.pushState({ path }, "", `/${path}`);
+  };
+
+  const goBack = () => {
+    if (state.length === 1) {
+      return;
+    }
+
+    state.pop();
+    window.history.back();
+  };
+
+  const currentRoute = () => state.at(-1);
+
+  window.addEventListener("popstate", () => {
+    state = [window.location.pathname.replace("/", "")];
+  });
+
+  return {
+    goTo,
+    goBack,
+    currentRoute,
+  };
+};
+
+const router = createRouter();
+
+const renderRoute = () => {
+  const appDiv = document.querySelector("#app");
+  const currentRoute = router.currentRoute();
+  const component = routes[currentRoute]?.();
+
+  if (!component) {
+    return appDiv.replaceChildren("404 not found");
+  }
+
+  appDiv.replaceChildren(component);
+};
+
+export { renderRoute, router };
