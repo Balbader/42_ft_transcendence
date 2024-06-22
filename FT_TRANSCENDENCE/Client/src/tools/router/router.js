c753869d11735ce3dde240c975467a9c56b24c3c
balbader <basilalbapro@gmail.com>
2024-06-22 13:27:16 +0200

    WIP

--- FT_TRANSCENDENCE/Client/src/tools/router/router.js
+++ FT_TRANSCENDENCE/Client/src/tools/router/router.js
@@ -1,1 +1,15 @@
-import { BrowserRoute } from "vanilla-routing";
+import { BrowserRoute } from 'vanilla-routing';
+
+// Define your routes here
+const routes = [
+	{
+		'pathname': '/',
+		element: () => document.createElement('div') {/* 👈 Renders at / */ }
+  },
+  {
+    'pathname': '/post',
+		element: () => document.createElement('div') {/* 👈 Renders at /post */ }
+  },
+];
+
+BrowserRoute(routes); // Initialize browser-based routing
