944e397f26a170649666697331c39ec44739822e
balbader <basilalbapro@gmail.com>
2024-06-23 10:30:31 +0200

    WIP

--- FT_TRANSCENDENCE/Client/src/my-app.js
+++ FT_TRANSCENDENCE/Client/src/my-app.js
@@ -1,36 +1,28 @@
-import "./views/profile-page/profile-page.js";
-import "./views/settings-page/settings-page.js";
-import "./views/pwd-change-page/password-change-page.js";
-import "./components/nav-bar/nav-bar-in.js";
-import "./components/footer/footer-in.js";
-import { LitElement, html, css } from "lit";
+import './views/profile-page/profile-page.js';
+import './views/settings-page/settings-page.js';
+import './views/pwd-change-page/password-change-page.js';
+import './components/nav-bar/nav-bar-in.js';
+import './components/footer/footer-in.js';
+import { LitElement, html } from 'lit';
 
 export class MyApp extends LitElement {
-  constructor() {
-    super();
-    this.isLoggedIn = {
-      // isLoggedIn: // ! Get auth response if user is logged in or not
-    };
-    this.navBar = {
-      // navBar: (navBar, isLoggedIn) => {
-      //   this.isLoggedIn
-      //     ? (navBar = <nav-bar-in></nav-bar-in>)
-      //     : (navBar = <nav-bar-out></nav-bar-out>);
-      //   return navBar;
-      // },
-    };
-  }
+	constructor() {
+		super();
+		this.isLoggedIn = {
+			// isLoggedIn: // ! Get auth response if user is logged in or not
+		};
+	}
 
-  render() {
-    return html`
-      <nav-bar-in></nav-bar-in>
+	render() {
+		return html`
+			<nav-bar-in></nav-bar-in>
 
-      <!--if (link pressed == pageName) -> load page -->
+			<!--if (link pressed == pageName) -> load page -->
 
-      <profile-page></profile-page>
+			<profile-page></profile-page>
 
-      <footer-in></footer-in>
-    `;
-  }
+			<footer-in></footer-in>
+		`;
+	}
 }
-customElements.define("my-app", MyApp);
+customElements.define('my-app', MyApp);
